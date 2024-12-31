731297506db797762dece506659b5cf71a27dd10
jaimin28 <jaiminkorat9597@gmail.com>
2024-12-31 17:24:02 +0530

    second commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <div>Portfolio sncs,mcns,nssnsnfsfnsm,fsnfm,sfn,smfns,fns,fnsf,snfsm,fns,mnf,smfns,fns,fnsf,sfn,</div>
   )
 }
 
